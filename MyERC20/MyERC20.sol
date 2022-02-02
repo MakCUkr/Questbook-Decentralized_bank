e44166a06fe91223a6972993cd53779cbd75be97
maksimjeet20566 <88977692+maksimjeet20566@users.noreply.github.com>
2022-02-02 10:37:53 +0200

    Mining
    Tokens will now be minted as per the price of MATIC. Price provided by Chainlink.

--- MyERC20/MyERC20.sol
+++ MyERC20/MyERC20.sol
@@ -1,9 +1,14 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+interface AggregatorV3Interface {
+    function latestRoundData() external view returns (uint80 roundId, uint answer, uint startedAt, uint updatedAt, uint80 answeredInRound)  ;
+}
+
+
 contract MyERC20
 {
-
+    AggregatorV3Interface internal priceFeed;
     string public NAME  = "VitalikNotABot";
     string public SYMBOL = "VNAB";
     mapping(address => uint) balances;
@@ -12,14 +17,13 @@ contract MyERC20
     uint public totalMined;
     address public owner;
 
-
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
-
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
     constructor()
     {
-        balances[msg.sender] += 1000* 1e8; 
+        // balances[msg.sender] += 1000 * 1e8; 
+        priceFeed = AggregatorV3Interface(0xFABe80711F3ea886C3AC102c81ffC9825E16162E);
         owner = msg.sender;
     }
 
@@ -61,7 +65,7 @@ contract MyERC20
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         require(balances[_from] > _value, "_from balance not enough");
         require(allowances[_from][msg.sender] >= _value, "allowance not enough");
-            
+
         balances[_from] -= _value;
         balances[_to] += _value;
         allowances[_from][msg.sender] -= _value;
@@ -79,14 +83,6 @@ contract MyERC20
         return allowances[_owner][_spender];
     }
     
-    function mine(address _receiver) public
-    {
-        require(msg.sender == owner);
-        balances[_receiver] += 10 * 1e8;
-        totalMined += 10 * 1e8;
-        require(totalMined <= totalSupply());
-    }
-
     function getBlockNumber() public view returns (uint num)
     {
         return block.number;
@@ -97,6 +93,37 @@ contract MyERC20
         return blocksMined[blockNumber];
     }
 
+    function mine() public payable returns (uint a) // the mint function prints (the amount fo MATIC you could get for the msg.value)/10 in VNAB tokens
+    {
+        uint coinsToBeMined = mintAmount(msg.value);
+        require (coinsToBeMined <= 1e13, "cant mint more than 1 lakh VNAB");
+        require((totalMined+coinsToBeMined)  <= totalSupply(), "can't mint tokens more than supply");
+
+        balances[msg.sender] += coinsToBeMined;
+        totalMined += coinsToBeMined;
+        
+        return coinsToBeMined;
+    }
+
+    function mintAmount(uint val) public view returns(uint weiAmount) 
+    {
+        uint a; uint tokenPrice; uint c;  uint d; uint e; uint tokenAmount;
+        (a, tokenPrice, c, d, e) = priceFeed.latestRoundData();
+        val  = val * 1e8 ;
+        val =  val / tokenPrice;
+        // val  = val * 1000000 * 1000000 * 1000000 ;
+        return val;
+    }
+
+    function receive() public payable
+    {
+        require(false, "tried to receive money but couldn't hehe");
+    }
+
 }
 
-// Contract address VNAB : 0xc18348E13D85bB121Ad509f2aA7db5A1c0b71254
+
+
+// Chainlink rinkeby MATIC/ETH Data Feed = 0x7794ee502922e2b723432DDD852B3C30A911F021
+// 1e8 = 100000000
+// 1e18 = 1000000000000000000 = 162 VNAB (by current price)
